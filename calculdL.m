e0c91fd6b5c641bd296b911acb8b57c010bb42a3
WilliamHdd <williamhdd@yahoo.fr>
2016-04-13 17:16:04 +0200

    adding some comments and description to the graph

--- calculdL.m
+++ calculdL.m
@@ -1,5 +1,9 @@
 function [L]=calculdL(T1,T2,l0,alpha)
+
+#convertion of your entries into Kelvin degrees and meters.
 t2 = T2 + 273.15;
 L0 = l0 * 1000;
+
+
 L = L0+ alpha * (t2 - T1 ) 
 endfunction
--- octave avril.m
+++ octave avril.m
@@ -1,8 +1,19 @@
+#Please enter the start and end temperature in celcius degrees.
 T1 = 20;
 T2 = 120;
+
+# Please enter the initial length of the object of your choice in milimeters.
+
 lo = 200;
+
+#Please enter the dilatation coefficient of the objet of your choice. This can be find on the internet if you don't know it.
 alpha = 1.6;
 
+title('Longueur finale en fonction de la témparature ')
+xlabel('Température')
+ylabel('Longueur finale')
+
+# The graph you'll get takes the temperature in Kelvin degrees on the X axis, and the lenght of your objet on the Y axis. This length is calculated by the function " calculdL".
 x = [T1+273.15:10:T2+273.15];
 y = calculdL(T1,x,lo,alpha);
 
