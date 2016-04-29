6bb73ed832426c8ce6522a6f094a63620045fa24
WilliamHdd <williamhdd@yahoo.fr>
2016-04-29 14:07:51 +0200

    Delete calculdL.m

--- calculdL.m
+++
@@ -1,9 +0,0 @@
-function [L]=calculdL(T1,T2,l0,alpha)
-
-#convertion of your entries into Kelvin degrees and meters.
-t2 = T2 + 273.15;
-L0 = l0 *10^(-3);
-
-
-L = L0*(1+ alpha * (t2 - T1 ) )
-endfunction
